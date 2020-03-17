ecf73d2e6d261a789eaebf93651ffa4f06e41311
adam <aitesting0@gmail.com>
2020-03-17 08:51:03 -1200

    making dynamic

--- COVID-19-Updates/Windows/covid19.sh
+++ COVID-19-Updates/Windows/covid19.sh
@@ -4,6 +4,6 @@ while :
 
 do 
 
-curl -s "https://thevirustracker.com/free-api?countryTotal=GB" | ./jq -j '.countrydata[0] | .info.title, ".\nTotal: ", .total_cases, "\tDeaths: ", .total_deaths, "\tRecovered: ", .total_recovered'
+curl -s "https://thevirustracker.com/free-api?countryTotal=$1" | ./jq -j '.countrydata[0] | .info.title, ".\nTotal: ", .total_cases, "\tDeaths: ", .total_deaths, "\tRecovered: ", .total_recovered'
 
 done
