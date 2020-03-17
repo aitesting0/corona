5087afd21788359cc93a4b450acec43520de8cee
adam <aitesting0@gmail.com>
2020-03-17 09:08:05 -1200

    adding 60 second sleep between updates and clearing terminal before each update

--- COVID-19-Updates/Windows/covid19.sh
+++ COVID-19-Updates/Windows/covid19.sh
@@ -5,5 +5,8 @@ while :
 do 
 
 curl -s "https://thevirustracker.com/free-api?countryTotal=$1" | ./jq -j '.countrydata[0] | .info.title, ".\nTotal: ", .total_cases, "\tDeaths: ", .total_deaths, "\tRecovered: ", .total_recovered'
-
+echo -e "\nThe terminal will be updated in 60 seconds with latest figures"
+sleep 60
+echo -e "\nUpdating and clearing terminal"
+clear
 done
