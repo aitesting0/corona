3d0cb165454d4faf909315639457354d8c02b3a7
adam <aitesting0@gmail.com>
2020-03-17 10:58:56 -1200

    for cat

--- COVID-19-Updates/Mac/covid19.sh
+++ COVID-19-Updates/Mac/covid19.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 while : 
-
 do 
 
-curl -s "https://thevirustracker.com/free-api?countryTotal=GB" | ./jq -j '.countrydata[0] | .info.title, ".\nTotal: ", .total_cases, "\tDeaths: ", .total_deaths, "\tRecovered: ", .total_recovered'
+curl -s "https://thevirustracker.com/free-api?countryTotal=$1" | ./jq -j '.countrydata[0] | .info.title, ".\nTotal: ", .total_cases, "\tDeaths: ", .total_deaths, "\tRecovered: ", .total_recovered'
+echo -e "\nThe terminal will be updated in 60 seconds with latest figures"
+
+sleep 60
+
+clear
 
 done
